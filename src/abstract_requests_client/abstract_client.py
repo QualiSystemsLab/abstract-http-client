a02f01a2764841e3f7d9c2d56717cb3568135381
katzy687 <katzy687@gmail.com>
2021-12-02 07:15:59 +0200

    updated doc string

--- src/abstract_requests_client/abstract_client.py
+++ src/abstract_requests_client/abstract_client.py
@@ -8,7 +8,7 @@ from abstract_requests_client.requests_service import RequestService
 
 
 class AbstractRequestsClient(ABC):
-    """ Inherit from this class to start writing your generic api """
+    """ Inherit from this class to start writing your api client """
 
     @abstractmethod
     def __init__(
